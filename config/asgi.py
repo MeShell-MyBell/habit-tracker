10e4504e2777315e2085d420c439de6accc197fd
Meshell-Mybell <shellie_w@live.com.au>
2025-07-28 13:26:02 +0100

    starter index and css

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for config project.
+ASGI config for habit_tracker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for config project.
+WSGI config for habit-tracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 

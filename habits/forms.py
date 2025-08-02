c8814349a271850bf077a459cb3849b79110eadd
Meshell-Mybell <shellie_w@live.com.au>
2025-08-02 11:51:01 +0100

    css update

--- habits/forms.py
+++ habits/forms.py
@@ -7,6 +7,8 @@ class HabitForm(forms.ModelForm):  # Renamed to HabitForm
         fields = ['name', 'description', 'category', 'completed']  # Removed 'created_at' as it is non-editable
 
         widgets = {
-            'description': forms.Textarea(attrs={'rows': 3}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter habit name'}),
+            'description': forms.Textarea(attrs={'rows': 3, 'class': 'form-control', 'placeholder': 'Enter habit description'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
-    
